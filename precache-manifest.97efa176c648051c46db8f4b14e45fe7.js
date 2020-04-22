060959d353add6bc168d6ec2e9a712e7f129408d
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2020-04-22 15:59:50 -0400

    Updates

---
+++ precache-manifest.97efa176c648051c46db8f4b14e45fe7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603188e8df740d34c507801142bc97cd",
+    "url": "/lumengraphics/index.html"
+  },
+  {
+    "revision": "e0c55ff03d47c28e9e90",
+    "url": "/lumengraphics/static/css/main.d41a801d.chunk.css"
+  },
+  {
+    "revision": "f036370b2276e1965ff2",
+    "url": "/lumengraphics/static/js/2.26bd4e0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lumengraphics/static/js/2.26bd4e0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c55ff03d47c28e9e90",
+    "url": "/lumengraphics/static/js/main.647b7d21.chunk.js"
+  },
+  {
+    "revision": "4213c919e7b538b20136",
+    "url": "/lumengraphics/static/js/runtime-main.8a2ae894.js"
+  },
+  {
+    "revision": "f2b01daa6feef83800e4b1e812b9fcc0",
+    "url": "/lumengraphics/static/media/apparel.f2b01daa.jpg"
+  },
+  {
+    "revision": "0b78ec9d509f67bfe3f8458c9d285df0",
+    "url": "/lumengraphics/static/media/avenir_light.0b78ec9d.otf"
+  },
+  {
+    "revision": "ceeb35bce357e899662426b61f52255b",
+    "url": "/lumengraphics/static/media/banners.ceeb35bc.jpg"
+  },
+  {
+    "revision": "c08b19a0939b3cc69f7f243b3e9efa50",
+    "url": "/lumengraphics/static/media/business_cards.c08b19a0.jpg"
+  },
+  {
+    "revision": "04f1d8728b781908558c8f1442c665ed",
+    "url": "/lumengraphics/static/media/instagram.04f1d872.svg"
+  },
+  {
+    "revision": "f891d9bdb5372ea285f4e9b38d4e90c3",
+    "url": "/lumengraphics/static/media/large_format_printing.f891d9bd.jpg"
+  },
+  {
+    "revision": "49770967dfef7a3a85c157c263d9a081",
+    "url": "/lumengraphics/static/media/logo.49770967.png"
+  },
+  {
+    "revision": "4de5adfbe706dfb5bdababb6d67f5aaa",
+    "url": "/lumengraphics/static/media/luminous_signs.4de5adfb.jpg"
+  },
+  {
+    "revision": "34756ec12cc720a5662d5adcc8ed287b",
+    "url": "/lumengraphics/static/media/promotional_products.34756ec1.jpg"
+  },
+  {
+    "revision": "d1fd167119e7214b2d586ca5010e8f41",
+    "url": "/lumengraphics/static/media/vehicle_wraps.d1fd1671.jpg"
+  },
+  {
+    "revision": "589c9ac58e351161827dbe77d6342fbc",
+    "url": "/lumengraphics/static/media/windows_and_walls_graphics.589c9ac5.jpg"
+  }
+]);
